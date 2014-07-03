zyyfly2010/learn-qf

Plane: support DGPS messages

ArduPlane/GCS_Mavlink.pde
@@ -2196,6 +2196,10 @@ mission_failed:
         }
         break;
 
+    case MAVLINK_MSG_ID_GPS_INJECT_DATA:
+        handle_gps_inject(msg, g_gps);
+        break;
+
     default:
         // forward unknown messages to the other link if there is one
         for (uint8_t i=0; i<num_gcs; i++) {

benizl/ardupilot

ArduPlane/GCS_Mavlink.pde
@@ -1529,12 +1529,14 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
             handle_log_message(msg, DataFlash);
         }
         break;
-
 #if HAL_CPU_CLASS > HAL_CPU_CLASS_16
     case MAVLINK_MSG_ID_SERIAL_CONTROL:
         handle_serial_control(msg, gps);
         break;
 #endif
+    case MAVLINK_MSG_ID_GPS_INJECT_DATA:
+        handle_gps_inject(msg, gps);
+        break;
 
     default:
         // forward unknown messages to the other link if there is one