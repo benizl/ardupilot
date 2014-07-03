zyyfly2010/learn-qf

GCS_MAVLink: added handle_gps_inject()

libraries/GCS_MAVLink/GCS.h
@@ -274,6 +274,8 @@ private:
     void handle_log_send(DataFlash_Class &dataflash);
     void handle_log_send_listing(DataFlash_Class &dataflash);
     void handle_log_send_data(DataFlash_Class &dataflash);
+    void handle_gps_inject(const mavlink_message_t *msg, GPS *gps);
+    
 
 };
 

benizl/ardupilot

libraries/GCS_MAVLink/GCS.h
@@ -323,6 +323,8 @@ private:
 
     // return true if this channel has hardware flow control
     bool have_flow_control(void);
+    void handle_gps_inject(const mavlink_message_t *msg, AP_GPS *gps);
+    
 };
 
 #endif // __GCS_H